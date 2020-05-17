c85b03fb84bf17bc09887efdeab2f6c4f912786c
Clebien <56209125+Clebien@users.noreply.github.com>
2020-05-17 16:51:01 +0200

    Update connection.blade.php

--- resources/views/connection.blade.php
+++ resources/views/connection.blade.php
@@ -1,75 +1,122 @@
+<!Doctype html>
 
-@extends('template3')
-
+@extends('layout')
+<head>
+    <title>Se connecter</title>
+    <style>
+        main{
+            padding-top:10em;
+        }
+    </style>
+</head>
 @section('contenu')
-    <div class="col-sm-offset-4 col-sm-4">
-        <br>
-        <div class="panel panel-info">  
-            <div class="panel-heading">Sign in</div>
-            <div class="panel-body"> 
-                 @if(isset($var))
-                  <div class="panel panel-danger">
-                  <div class="panel-heading">{{ $var }} cliquez <a href = "/inscription">ici pour vous inscrire</a></div>
-                  </div>
-                 @endif
-                 @if(isset($err))
-                  <div class="panel panel-danger">
-                  <div class="panel-heading">{{ $err }}</div>
-                  </div>
+<body>
 
-                 @endif
-                    {!! Form::open(['url' => 'user/connection', 'method' => 'post']) !!}
-                    {{csrf_field()}}
-                    <div class="col-sm-12">
-                      
-                        <label for="name">Email<span>*</span>
-                              <div class ="form-group">
-                                  <input type="email" name="email" placeholder="Email" value="{{ old('email') }}" size="50%">
-                                  <br>
-                                  {!! $errors->first('email', '<small class="text-danger">:message</small>') !!}
-                              </div> 
-                        </label>
-                        <br>
-                        <label for="name">Mot de passe<span>*</span>
-                              <div class ="form-group">
-                                  <input type="password" name="password" placeholder="Mot de passe" size="50%" >
-                                  <br>
-                                  {!! $errors->first('password', '<small class="text-danger">:message</small>') !!}
-                              </div>
-                        </label>
-                        <br>
-                        <label for="name">Confirmation du mot de passe<span>*</span>
-                              <div class ="form-group"> 
-                                  <input type="password" name="password_confirmation" placeholder="Retaper le mot de passe" size="50%" >
-                                  <br>
-                                  {!! $errors->first('password_confirmation', '<small class="text-danger">:message</small>') !!}
-                              </div>
-                              @if(isset($passerr))
-                                <div class="panel panel-danger">
-                                <div class="panel-heading">{{$passerr}}</div>
-                                </div>
+<main>
+<div class="container">
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <div class="panel panel-default">
+                <div class="panel-heading" align="center">Connection</div>
+                <div class="panel-body">
+                 
+                    {!! Form::open(['url' => 'user/connection', 'method' => 'post','class'=>'form-horizontal']) !!}
+
+                        {!! csrf_field() !!}
+              
+                        @if(session('var'))
+                          <div class="panel panel-danger">
+                          <div class="panel-heading" align="center">{{ session('var') }} cliquez <a href = "/inscription">ici pour vous inscrire</a></div>
+                          </div>
+                        @endif
+                        @if(session('err'))
+                          <div class="panel panel-danger">
+                          <div class="panel-heading" align="center">{{ session('err')}}</div>
+                          </div>
+
+                        @endif
+                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
+                            <label class="col-md-4 control-label">Adresse mail</label>
+
+                            <div class="col-md-6">
+                                <input type="email" class="form-control" name="email" value="{{ old('email') }}">
+                                @if ($errors->has('email'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('email') }}</strong>
+                                    </span>
+                                @endif
+                                @if(session('error'))
+                              <span class="help-block" align="center">
+                                <strong>{{ session('error') }}</strong>
+                              </span>
                               @endif
-                        </label>
-                      <div class="form-group">
+                            </div>
+                        </div>
+
+                        <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
+                            <label class="col-md-4 control-label">Mot de passe</label>
+
+                            <div class="col-md-6">
+                                <input type="password" class="form-control" name="password">
+
+                                @if ($errors->has('password'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('password') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
+                        <div class="form-group{{ $errors->has('password_confirmation') ? ' has-error' : '' }}">
+                            <label class="col-md-4 control-label">Confirmation du mot de passe</label>
+
+                            <div class="col-md-6">
+                                <input type="password" class="form-control" name="password_confirmation">
+
+                                @if ($errors->has('password_confirmation'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('password_confirmation') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+                        <div align="center">
+                        <div class="form-group{{ $errors->has('statut') ? ' has-error' : '' }}">
                             <div class="checkbox">
                             <label>
-                              <input type="radio" name="statut" value="1" >Etudiant<span>*</span>
-                              @if($errors->has('statut'))
-                                    <p><span><small>{{ $errors->first('statut') }}</small></span></p>
-                                  @endif
+                              <input type="radio" name="statut" value="1" >Etudiant
+                                  @if ($errors->has('statut'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('statut') }}</strong>
+                                    </span>
+                                @endif
                             </label>
                             <label align="right">
-                              <input type="radio" name="statut" value="2">Enseignant<span>*</span>
-                              @if($errors->has('statut'))
-                                    <p><span><small>{{ $errors->first('statut') }}</small></span></p>
-                                  @endif
+                              <input type="radio" name="statut" value="2">Enseignant
+                              @if ($errors->has('statut'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('statut') }}</strong>
+                                    </span>
+                                @endif
                             </label>
                             </div>
                       </div>
-                    {!! Form::submit('Envoyer', ['class' => 'btn btn-info pull-right']) !!}
+                      </div>
+                        <div class="form-group" align="center">
+                            <div class="col-md-6 col-md-offset-4">
+                                <button type="submit" class="btn btn-primary">
+                                    Connection
+                                </button>
+                            </div>
+                        </div>
                     {!! Form::close() !!}
                 </div>
             </div>
         </div>
-
+    </div>
+</div>
+<main>
+<body>
 @endsection
+</html>
+
