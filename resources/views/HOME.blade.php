d1ad31d87c9df8c49ff0d79be463c1de80522b7c
Clebien <56209125+Clebien@users.noreply.github.com>
2020-05-19 09:21:12 +0200

    Update HOME.blade.php

--- resources/views/HOME.blade.php
+++ resources/views/HOME.blade.php
@@ -15,6 +15,13 @@
       <div class="jumbotron text-center">
       <h1>UNIVERSITE PARIS NANTERRE</h1>
       <br>
+      <P>
+            Bienvenu, ce site permet de getion de la Miage Nanterre <br>
+            -Les étudiants pouront s'inscrire, créer leur identifiants, et candidater à une formation <br>
+            -Les enseignants pourront étudier les dossiers des étudiants depuis leur espace personnelle <br>
+            -L’administrateur (login : admin@parisnanterre.fr / mot de passe : admin) via un formulaire, pourra insérer dans la base une liste d'enseignant.
+      </P>
+      <br>
         <a href="admin" class="btn btn-primary my-2">Administrateur</a>
         <a href="/inscription" class="btn btn-warning my-2">Etudiant / Enseignant</a>
       </p> 
