8612acc0347d90d3fea470d9f7e67910ebb64b46
Clebien <56209125+Clebien@users.noreply.github.com>
2020-05-19 09:23:45 +0200

    Update validation.blade.php

--- resources/views/validation.blade.php
+++ resources/views/validation.blade.php
@@ -39,7 +39,7 @@
                         </div>
 
                         <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                            <label class="col-md-4 control-label">Mot de passe</label>
+                            <label class="col-md-4 control-label">Nouveau mot de passe</label>
 
                             <div class="col-md-6">
                                 <input type="password" class="form-control" name="password">
